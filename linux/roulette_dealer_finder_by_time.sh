6c580282e3be7b2647389f4c51070d9100819706
andrewfox201 <andrewfox201@gmail.com>
2021-12-11 21:05:56 +0000

    add scripts to linux folder

---
+++ linux/roulette_dealer_finder_by_time.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat $1_Dealer_schedule | grep $2 | grep $3 | awk -F " " '{print $1,$2,$5,$6}'
+
+
+#3arguements
+#$1 is the four digits of the date name in front of schedule i.e. 0310
+#$2 is the time i.e. 07:00
+#$3is AM or PM i.e. AM
+
